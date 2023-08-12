f3de251b4a44daeb59ca8e036ca0ba0d7505c4ca
3mr3yyad <3mr3yyad@gmail.com>
2023-08-12 11:00:40 -0700

    finished

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Description: all possible combinations of two two-digit numbers
+ * Return: 0 = success
+*/
+int main(void)
+{
+	int x = 0, y;
+	while (x <= 99)
+	{
+		x = y;
+		while (y <= 99)
+		{
+			if(x != z)
+			{
+			
+			}
+		y++;
+		}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
