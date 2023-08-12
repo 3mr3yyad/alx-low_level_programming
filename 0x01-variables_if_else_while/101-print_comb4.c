308c4ee979988632492c61a83e7e87518e51d2e5
3mr3yyad <3mr3yyad@gmail.com>
2023-08-12 10:37:19 -0700

    finished

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Description: possible different combinations of two digits
+ * Return: 0 = success
+*/
+int main(void)
+{
+	int x = 0, y, z;
+	/* loop starting point  */
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			z = 0;
+			while (z <= 9)
+			{
+				if (x != y && x < y && y != z && y < z)
+				{
+				putchar(x + 48);
+				putchar(y + 48);
+				putchar(z + 48);
+					if (x != 8 ||  y != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
