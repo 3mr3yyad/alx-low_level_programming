1452428d62c3ecefe575ec06ade1d571f061b9ad
3mr3yyad <3mr3yyad@gmail.com>
2023-08-11 18:24:54 -0700

    finished

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * Description: alphabet in lower case
+ * Return: 0 = success
+*/
+int main(void)
+{
+	char abc = 'z';
+
+	while (abc >= 'a')
+	{
+		putchar(abc);
+		++abc;
+	}
+	putchar('\n');
+	return (0);
+}
