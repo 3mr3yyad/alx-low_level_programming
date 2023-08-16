8df9f995ab955498176a17b40e5c19ad2c315a7b
3mr3yyad <3mr3yyad@gmail.com>
2023-08-15 20:02:02 -0700

    finished

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * tt - print 9 taples
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 2, ..
+*/
+void tt(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			/*
+			 * put space if 1
+			 * place first id 2
+			*/
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get first*/
+			_putchar((prod % 10) + 48); /*get second*/
+		}
+		_putchar('\n');
+	}
+}
