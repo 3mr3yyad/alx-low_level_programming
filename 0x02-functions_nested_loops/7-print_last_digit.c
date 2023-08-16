7f4c5447568b524c9bda5af67331b7bbf63bd9c5
3mr3yyad <3mr3yyad@gmail.com>
2023-08-15 19:36:29 -0700

    finished

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - start run
+ * @n: prints num
+ * Return: ld = success
+*/
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	if (n < 0)
+		ld = -1 * (n % 10);
+	else
+		ld = n % 10;
+	_putchar(ld = '0');
+	return (ld);
+}
