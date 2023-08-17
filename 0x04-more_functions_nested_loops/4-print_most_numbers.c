2c22cfb2069629ff0bcdb6f387bbc941b2338eed
3mr3yyad <3mr3yyad@gmail.com>
2023-08-17 15:15:39 -0700

    finished

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - main
+ * Description: function that prints the numbers followed by a new line
+ * Return: always 0
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x >= 0; x <= 9; x++)
+	{
+		if (x == 4 || x == 2)
+			continue;
+		_putchar(x + 48);
+	}
+	_putchar('\n');
+}
