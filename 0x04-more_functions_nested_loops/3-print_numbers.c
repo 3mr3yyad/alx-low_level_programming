a5378791db77dae72e80164ceee536a905ee5cc8
3mr3yyad <3mr3yyad@gmail.com>
2023-08-17 14:51:16 -0700

    finished

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - one to nine
+ * Description: print numbers
+ * Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int x = 0;
+
+	do {
+		_putchar(x + 48);
+		x++;
+	}
+	while (x >= 0 && x < 10)
+		_putchar('\n');
+}
