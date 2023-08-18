1c26921bf45b614de9d13dd2e0460a48757c1e95
3mr3yyad <3mr3yyad@gmail.com>
2023-08-17 18:18:16 -0700

    finished

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * squareRoot - main
+ * @x: input
+ * Return: root of x
+*/
+
+double squareRoot(double x)
+{
+	float sqrt, tmp
+
+	tmp = 0;
+	sqrt = x / 2;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+ * greatest - main
+ * @num: input2
+*/
+
+void greatest(long int num)
+{
+	int pr, gr;
+
+	/* while loop */
+	while (num % 2 == 0)
+		num = num / 2;
+
+	/* rule */
+	for (pr = 3; pr <= squareRoot(num); pr += 2)
+	{
+		while (num % pr == 0)
+		{
+			num = num / pr;
+			gr = pr;
+		}
+	}
+
+	if (num > 2)
+		gr = num;
+	printf("%d\n", gr);
+}
+
+/**
+ * main - start
+ * Description: final step
+ * Return: always 0
+*/
+
+int main(void)
+{
+	greatest(612852475143);
+
+	return (0);
+}
