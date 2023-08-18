5d3c963d172b0d4d39c8cfecd13beca976012100
3mr3yyad <3mr3yyad@gmail.com>
2023-08-17 18:59:18 -0700

    finished

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,19 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n <= 0)
-		_putchar('\n');
-
-	for (x = 1; x < n; x++)
 	{
-		for (y = 1; y < x; y++)
-		{
-			_putchar(' ');
-		}
-		_putchar(95);
 		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x < n; x++)
+		{
+			for (y = 1; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar(95);
+			_putchar('\n');		
+		}
 	}
 }
