3659c9f56088f3edb6c190c541f4116f3c78648b
3mr3yyad <3mr3yyad@gmail.com>
2023-08-21 12:38:24 -0700

    finished

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - main
+ * @s: input
+ * Return: NULL
+*/
+
+void rev_string(char *s)
+{
+	int x,  y;
+	char z;
+
+	for (x = 0; s[x] != '\0'; ++x)
+		;
+
+	for (x = 0; x < y / 2; x++)
+	{
+		z = s[x];
+		s[x] = s[y - 1 - x];
+		s[y - 1 - x] = z;
+	}
+}
