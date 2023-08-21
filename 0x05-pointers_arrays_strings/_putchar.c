d62b8c99db713e33409fb55761d3ea0d924ddb03
3mr3yyad <3mr3yyad@gmail.com>
2023-08-21 11:38:24 -0700

    finished

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - write the character c
+ * @c: print character
+ * Return: 1 = success
+ *	-1 no error
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
