d459594060949f38a1181ac1b1a951c7581c613d
3mr3yyad <3mr3yyad@gmail.com>
2023-08-22 15:45:04 -0700

    finished

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password
+ * Description: for crackme
+ * Retern: always 0
+*/
+
+int main(void)
+{
+	int x;
+	char c;
+
+	srand(time('\0'));
+	
+}
