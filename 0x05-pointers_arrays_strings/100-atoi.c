f71fafe001b52c60f164c35cce0a1202df066a32
3mr3yyad <3mr3yyad@gmail.com>
2023-08-22 15:22:47 -0700

    finished

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - main
+ * @s: input
+ * Return: int
+*/
+
+int _atoi(char *s)
+{
+	unsigned int = ux;
+	int x = 0;
+
+	do {
+		if (*s == '-')
+			x *= -1;
+		else if (*s >= '0' && < '10')
+			ux = (ux * 10) + (*s - '0');
+		else if (ux > 0)
+			break;
+	} while (*s++);
+
+	return (ux * x);
+}
