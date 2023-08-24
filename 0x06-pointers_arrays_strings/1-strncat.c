67e3d9a393480d0b5c41a91e3bc28621af7eca7e
3mr3yyad <3mr3yyad@gmail.com>
2023-08-23 18:02:30 -0700

    finished

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - main
+ * @dest: input1
+ * @src: input2
+ * @n: input3
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+
+	while (dest[x])
+		x++;
+
+	for (y = 0; y < n && src[y] != '\0'; y++)
+		dest[x + y = src[y];
+	dest[x + y] = '\0';
+
+	return (dest);
+}
