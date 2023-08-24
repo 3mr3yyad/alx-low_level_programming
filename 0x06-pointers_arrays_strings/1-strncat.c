63b6288e0737ec5cfbd4357ea208224af42919d1
3mr3yyad <3mr3yyad@gmail.com>
2023-08-23 18:05:37 -0700

    finished

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,8 +18,8 @@ char *_strncat(char *dest, char *src, int n)
 		x++;
 
 	for (y = 0; y < n && src[y] != '\0'; y++)
-	{	dest[x + y] = src[y];
+		dest[x + y] = src[y];
 	dest[x + y] = '\0';
-	}
+	
 	return (dest);
 }
