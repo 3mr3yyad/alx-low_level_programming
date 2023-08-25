068418b3e282f17ba0da0b044e71d14dacd4035f
3mr3yyad <3mr3yyad@gmail.com>
2023-08-24 18:43:39 -0700

    finished

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * infinite_add - main
+ * @n1: input
+ * @n2: input2
+ * @r: input3
+ * @size_r: input4
+ * Return: sum
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * print_buffer - main
+ * @b: input
+ * @size: input2
+ * Return:
+ *
