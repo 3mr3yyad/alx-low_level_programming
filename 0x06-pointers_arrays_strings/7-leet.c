2ee799daeba11f024fb46b868f2a10e4ff265b47
3mr3yyad <3mr3yyad@gmail.com>
2023-08-24 17:44:03 -0700

    finished

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 
 char *leet(char *a)
 {
-	char v = a;
+	char *v = a;
 	char l[] = {'A', 'E', 'O', 'T', 'L'};
 	int n[] = {4, 3, 0, 7, 1};
 	unsigned int x;
@@ -19,9 +19,7 @@ char *leet(char *a)
 		for (x = 0; x < sizeof(l) / sizeof(char); x++)
 		{
 			if (*a == l[x] || *a == l[x] + 32)
-			{
 				*a = 48 + n[x];
-			}
 		}
 		a++;
 	}
