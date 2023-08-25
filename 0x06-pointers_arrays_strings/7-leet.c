c4136f9305e7cad9654ea5e4d53f765372f594b2
3mr3yyad <3mr3yyad@gmail.com>
2023-08-24 17:38:24 -0700

    finished

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - mozart was genuis
+ * @leet: @a
+ * @a: Leet
+ * Return: v
+*/
+
+char *leet(char *a)
+{
+	char v = a;
+	char l[] = {'A', 'E', 'O', 'T', 'L'};
+	int n[] = {4, 3, 0, 7, 1};
+	unsigned int x;
+
+	while (*a)
+	{
+		for (x = 0; x < sizeof(l) / sizeof(char); x++)
+		{
+			if (*a == l[x] || *a == l[x] + 32)
+				*a = 48 + n[x];
+		}
+		a++;
+	}
+	return (v);
+}
