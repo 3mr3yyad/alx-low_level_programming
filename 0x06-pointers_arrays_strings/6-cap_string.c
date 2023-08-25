53a88574e1fadb1fe4fd3e99cd3eb4332e54fe6c
3mr3yyad <3mr3yyad@gmail.com>
2023-08-24 17:23:43 -0700

    finished

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * lc - main
+ * @a: input
+ * Return: 1 = true, 0 = false
+*/
+
+int lc(char a)
+{
+	return (a >= 97 && a <= 122);
+}
+
+/**
+ * id - main
+ * @a: input
+ * Return: 1 = true, 0 = false
+*/
+
+int id(char a)
+{
+	int x;
+	char dd[] = " \t\n,;.!?\"(){}";
+
+	for (x = 0; x < 13; x++)
+		if (a == dd[x])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - main
+ * @z: input
+ * Return: o
+*/
+
+char *cap_string(char *z)
+{
+	int fd = 1;
+	char *o = z;
+
+	while (*z)
+	{
+		if (id(*z))
+			fd = 1;
+		else if (lc(*z) && fd)
+		{
+			*z -= 32;
+			fd = 0;
+		}
+		else
+			fd = 0;
+		z++;
+	}
+	return (o);
+}
