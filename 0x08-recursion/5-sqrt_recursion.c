8d588996d7f27d59a134de8f87d8c2938b29b15a
3mr3yyad <3mr3yyad@gmail.com>
2023-08-30 16:52:56 -0700

    done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - main
+ * @n: input
+ * @x: input1
+ * Return: _sqrt_recursion
+*/
+
+int sq(int n, int x);
+int _sqrt_recursion(int n)
+{
+	return (sq(n, 1));
+}
+
+/**
+ * sq - main2
+ * @n: iput
+ * @x: input1
+ * Return: same
+*/
+
+int sq(int n, int x)
+{
+	if (x * X == n)
+	{
+		return (x);
+	}
+	else if (x * X < n)
+	{
+		return (sq(n, x + 1));
+	}
+	else
+	{
+	return (-1);
+	}
+}
