cb2567c316b4e0fb804b8331e9f1e2f1d1e08453
3mr3yyad <3mr3yyad@gmail.com>
2023-08-30 16:00:09 -0700

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <strlen>
+
+/**
+ * _strlen_recursion - main
+ * @s: input
+ * Return: @x
+*/
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s > '\0')
+	{
+		x += _strlen_recursion(s + 1) + 1;
+	}
+	return (x);
+}
