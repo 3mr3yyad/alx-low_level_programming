8af17f67540f94cf1022aca8cb66a0bc2de67978
3mr3yyad <3mr3yyad@gmail.com>
2023-09-01 05:17:40 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - whatsmyname
+ * @argc: input
+ * @argv: input1
+ * Return: always 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
