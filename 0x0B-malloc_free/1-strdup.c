fdcab7b2685710a378e3302fee2a70409976a8bf
3mr3yyad <3mr3yyad@gmail.com>
2023-09-05 18:25:15 -0700

    done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - main
+ * @str: input
+ * Return: 0 || Null
+ */
+
+char *_strdup(char *str)
+{
+	int x = 0, y = 0;
+	char *c;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[y] != '\0'; y++)
+		;
+
+	c = malloc(y * sizeof(*str) + 1);
+
+	if (c == 0)
+		return (NULL);
+
+	else
+	{
+		for (; x < y; x++)
+			c[x] = str[x];
+	}
+	return (c)
+}
