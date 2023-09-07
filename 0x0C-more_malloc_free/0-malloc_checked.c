7eac7263ea936be5ed1be83991cb2efb9e9eac22
3mr3yyad <3mr3yyad@gmail.com>
2023-09-07 13:37:58 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - main
+ * @b: input
+ * Return: 0
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *x = malloc9b0;
+
+	if (x == 0)
+		exit(98);
+	return (x);
+}
