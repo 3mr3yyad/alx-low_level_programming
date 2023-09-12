d6ff1073eda3812fa5f01bf6141e42e5e0ec579f
3mr3yyad <3mr3yyad@gmail.com>
2023-09-12 07:40:41 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,7 +44,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	for (x = 0; x < olen; x++)
+	for (x = 0; x < oln; x++)
 		doggy->owner[x] = owner[x];
 
 	return (doggy);
