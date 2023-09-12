33d1de49860e1d7b5e66a7d8c02b620933e35c33
3mr3yyad <3mr3yyad@gmail.com>
2023-09-11 18:09:29 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - main
+ * @name: input
+ * @age: input1
+ * @owner: input2
+ * Description: Define a new type
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
