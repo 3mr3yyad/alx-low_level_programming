f3acffb321d7c3cd1b9f85c6d318fa8a56047783
3mr3yyad <3mr3yyad@gmail.com>
2023-09-11 18:53:09 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,6 @@ struct dog
 }
 ;
 
-#endif
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
