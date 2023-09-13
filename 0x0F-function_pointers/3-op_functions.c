592af7a34471c19b9acf7e0f5603493bba709427
3mr3yyad <3mr3yyad@gmail.com>
2023-09-13 12:59:57 -0700

    done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - main
+ * @a: input
+ * @b: input1
+ * Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - main
+ * @a: input
+ * @b: input1
+ * Return: sub
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - main
+ * @a: input
+ * @b: input1
+ * Return: mul
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - main
+ * @a: input
+ * @b: input1
+ * Return: mul
+*/
+
+int op_div(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - main
+ * @a: input
+ * @b: input1
+ * Return: div
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
