4fc59a83b33aa04eeb11619d58ecf76b343be5d4
3mr3yyad <3mr3yyad@gmail.com>
2023-09-13 13:46:03 -0700

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,24 +7,31 @@
  * Return: 0
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int), a, b;
+	int x, y, result;
+	int (*p)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	a = atoi(argc[1]);
-	b = atoi(argv[3]);
+	x = atoi(argc[1]);
+	y = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("Error\n"), exit(99);
+	p = get_op_func(argv[2]);
 
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(100);
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	printf("%d\n", op_func(a, b));
+	result = p(x, y);
+
+	printf("%d\n", result);
 
 	return (0);
 }
