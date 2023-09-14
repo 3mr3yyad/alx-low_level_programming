6727de2777a5e2badd559ee99fdd7c641ce9379d
3mr3yyad <3mr3yyad@gmail.com>
2023-09-14 06:19:52 -0700

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - main
+ * @n: input
+ * @...: input1
+ * Return: sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int a = 0, x = n;
+	va_list y;
+
+	if (!n)
+		return (0);
+
+	va_start(y, n);
+	while (x--)
+		a += va_arg(y, int);
+	va_end(y);
+	return (a);
+}
