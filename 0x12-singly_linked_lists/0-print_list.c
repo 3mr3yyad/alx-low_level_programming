42fe6111d8d801077f8d881a928d9b3e4ef9dcd5
3mr3yyad <3mr3yyad@gmail.com>
2023-09-21 09:20:55 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * strlng - main
+ * @c: input
+ * Return: x
+*/
+
+int strlng(char *c)
+{
+	int x = 0;
+
+	if (!c)
+		return (0);
+
+	while (*c++)
+		x++;
+
+	return (x);
+}
+
+/**
+ * print_list - main
+ * @h: input
+ * Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlng(h->str), ? h->str : "(nil)");
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
