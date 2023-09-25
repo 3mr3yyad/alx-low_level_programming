14ee7e1a3deafac94cc29f4791bae0606e475695
3mr3yyad <3mr3yyad@gmail.com>
2023-09-25 12:54:48 -0700

    done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,21 +3,21 @@
 /**
  * pop_listint - main
  * @head: head node
- * Return: x
+ * Return: i
 */
 
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
-	int x;
+	int i;
 
 	if (!head || !*head)
 		return (0);
 
 	node = (*head)->next;
-	x = (*head)->x;
+	i = (*head)->i;
 	free(*head);
 	*head = node;
 
-	return (x);
+	return (i);
 }
