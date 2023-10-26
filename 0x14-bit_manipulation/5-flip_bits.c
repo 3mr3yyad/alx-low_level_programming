511bf58ec6e6ea5aee5f1c2851333f806d23f65a
3mr3yyad <3mr3yyad@gmail.com>
2023-10-26 16:44:32 -0700

    here we go again

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - main
+ * @n: input
+ * @m: input1
+ * Return: a
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int a = 0;
+
+	while (x)
+	{
+		if (x & 1ul)
+			a++;
+		x = x >> 1;
+	}
+	return (a);
+}
